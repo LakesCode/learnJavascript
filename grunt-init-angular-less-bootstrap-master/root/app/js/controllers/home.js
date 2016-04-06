c7c6525e491550d554a7436bc3abf9e3de0f25c2
donghaitao <280038780@qq.com>
2016-04-06 16:42:45 +0800

    Create home.js

---
+++ grunt-init-angular-less-bootstrap-master/root/app/js/controllers/home.js
@@ -0,0 +1,11 @@
+/*jslint es5: true, devel: true, node: true, indent: 2, vars: true, white: true, nomen: true */
+/*global angular*/
+
+angular.module('test').controller('home', function (
+  $scope
+) {
+  'use strict';
+  
+  
+  
+});
